07da8fda0b84f982845b9b727e0f716da1322319
aminjonshermatov <aminjonshermatov@gmail.com>
2026-05-17 17:28:11 +0000

    bounded_queue: bench

--- thread_pool/concurrent_queue/bounded_queue/queue_bench.cpp
+++ thread_pool/concurrent_queue/bounded_queue/queue_bench.cpp
@@ -2,14 +2,31 @@
 
 #include <benchmark/benchmark.h>
 
+#include "thread_pool/base/compiler_traits.hpp"
+
 using namespace TP_NAMESPACE;
 
-static void BMNonBlockingQueue(benchmark::State& state) {
-  using Task = std::move_only_function<void()>;
-  bounded::Queue<Task, LOG_SIZE> queue;
+static void BM_BoundQueue_Sequential(  // NOLINT(readability-identifier-naming)
+    benchmark::State& state) {
+  constexpr auto kLogSize = 16;
+  constexpr auto kOperationsCount = 50'000;
+
+  struct Int {
+    int Val = 0;
+    Int(const int val) : Val(val) { }
+  };
+
+  bounded::Queue<Int, kLogSize> queue;
   for (TP_MAYBE_UNUSED auto _ : state) {  // NOLINT(readability-identifier-length)
+    for (int iter = 0; iter < kOperationsCount; ++iter) {
+      benchmark::DoNotOptimize(queue.TryPush(Int(iter)));
+    }
+    for (auto iter = 0U; iter < kOperationsCount; ++iter) {
+      benchmark::DoNotOptimize(queue.Pop());
+    }
     benchmark::DoNotOptimize(queue.Size());
   }
+  state.SetItemsProcessed(state.iterations() * kOperationsCount * 2);
 }
 
-BENCHMARK(BMNonBlockingQueue);
+BENCHMARK(BM_BoundQueue_Sequential);
